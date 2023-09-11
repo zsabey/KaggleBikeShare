5353e9ce529f8866b8ad3021e9e66cf7d1dd20ab
Zach <zacharysabey@gmail.com>
2023-09-11 17:12:54 -0600

    Added EDA to train data

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -1,3 +1,46 @@
 ##
-library(tidyverse)
 
+library(tidyverse)
+library(vroom)
+library(patchwork)
+
+sampleSub <- vroom('sampleSubmission.csv')
+
+testCsv <- vroom('test.csv')
+
+trainCsv <- vroom('train.csv')
+
+trainCsv
+
+dplyr::glimpse(trainCsv)
+
+skimr::skim(trainCsv)
+
+view(trainCsv)
+
+trainCsv$season <- as.factor(trainCsv$season)
+trainCsv$holiday <- as.factor(trainCsv$holiday)
+trainCsv$workingday <- as.factor(trainCsv$workingday)
+trainCsv$weather <- as.factor(trainCsv$weather)
+
+
+DataExplorer::plot_intro(trainCsv)
+corrPlot1 <- DataExplorer::plot_correlation(select(trainCsv, count, windspeed, humidity, atemp, temp))
+DataExplorer::plot_bar(trainCsv)
+
+histPlot1 <- DataExplorer::plot_histogram(trainCsv)
+
+DataExplorer::plot_missing(trainCsv)
+GGally::ggpairs(trainCsv)
+
+tempvCountPlot <- ggplot(data=trainCsv) +
+  geom_point(mapping=aes(x=temp,y=count, color = season)) +
+  geom_smooth(mapping=aes(x=temp,y=count, color = season),se=FALSE)
+
+weathervCountPlot <- ggplot(data=trainCsv) + 
+  geom_boxplot(mapping=aes(x=weather,y=count))
+
+(corrPlot1 + histPlot1) / (tempvCountPlot + weathervCountPlot)
+
+ggplot(data=trainCsv) +
+  geom_point(mapping=aes(x=humidity,y=count))            
